ba4aff729591dbce5ece0ca4e93177bff67753f0
florian-savoie <florianpsp303@gmail.com>
2024-04-14 12:09:23 +0300

    template

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma Page</title>
+    <!-- Inclusion de Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/cerulean/bootstrap.min.css" rel="stylesheet">
+    <!-- Inclusion de jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Inclusion de Bootstrap JS (facultatif, mais nécessaire pour certaines fonctionnalités) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+    <h1>Ma Page HTML avec Bootstrap et jQuery</h1>
+</div>
+
+
+</body>
+</html>
